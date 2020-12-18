2e9890a77f3a751e7d9de4bb60daae7839fa7679
piyushverma99 <76200905+piyushverma99@users.noreply.github.com>
2020-12-18 13:56:40 +0530

    Add files via upload

---
+++ Coursera_Capstone.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ### Data Science Capstone Project 
+# This is the first project of the course Applied Data Science Capstone
+
+# In[1]:
+
+
+import pandas as pd
+import numpy as np
+
+
+# In[2]:
+
+
+print('Hello Capstone Project Course!')
+
+
+# In[ ]:
+
+
+
+
